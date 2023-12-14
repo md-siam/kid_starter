c4f353a35220b655666ac9ddc307ea60be7ccaf3
Amirul Islam <amirul@mislbd.com>
2023-12-15 00:11:29 +0600

    animal screen add

--- lib/app/widgets/image_card.dart
+++ lib/app/widgets/image_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import '../constant.dart';
 
@@ -6,10 +7,11 @@ class ImageCard extends StatelessWidget {
   final bool isActive;
   final String title;
   final String image;
+  final Color textColor;
   final Color backgroundColor;
   final double fontSizeBase;
   final double fontSizeActive;
-  
+
   final VoidCallback onTap;
 
   const ImageCard({
@@ -17,9 +19,10 @@ class ImageCard extends StatelessWidget {
     this.isActive = false,
     required this.title,
     required this.image,
+    this.textColor = kTitleTextColor,
     this.backgroundColor = Colors.white,
-    this.fontSizeBase = 130,
-    this.fontSizeActive = 180,
+    this.fontSizeBase = 20,
+    this.fontSizeActive = 22,
     required this.onTap,
   }) : super(key: key);
 
@@ -44,19 +47,51 @@ class ImageCard extends StatelessWidget {
         type: MaterialType.transparency,
         child: InkWell(
           onTap: onTap,
-          child: Container(
-            alignment: Alignment.center,
-            child: AnimatedDefaultTextStyle(
-              style: isActive
-                  ? TextStyle(fontSize: fontSizeActive)
-                  : TextStyle(fontSize: fontSizeBase),
-              duration: const Duration(milliseconds: 400),
-              curve: Curves.bounceOut,
-              child: Image.asset(
-                image,
-                fit: BoxFit.cover,
+          child: Stack(
+            children: [
+              Container(
+                alignment: Alignment.center,
+                child: AnimatedDefaultTextStyle(
+                  style: isActive
+                      ? TextStyle(fontSize: fontSizeActive)
+                      : TextStyle(fontSize: fontSizeBase),
+                  duration: const Duration(milliseconds: 400),
+                  curve: Curves.bounceOut,
+                  child: Image.asset(
+                    image,
+                    fit: BoxFit.cover,
+                  ),
+                ),
               ),
-            ),
+              Positioned(
+                bottom: 0,
+                right: 0,
+                left: 0,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      color: backgroundColor.withOpacity(0.5),
+                    ),
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Text(
+                        title,
+                        style: GoogleFonts.roboto(
+                          textStyle: TextStyle(
+                            color: textColor,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                        softWrap: false,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
